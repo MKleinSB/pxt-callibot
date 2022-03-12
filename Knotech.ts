08ac73ffb9eae94faee8e91a663a24245d86e216
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2022-03-12 08:38:22 +0100

    Update auf Callibot2

--- Knotech.ts
+++ Knotech.ts
@@ -1,54 +1,42 @@
-// V1.0.14
-// Hinweis: Die Fernsteuer-Befehle wurden entfernt, weil die hierfür nötige "FUNK"
-// Bibliothek nicht kompatibel zur "Bluetooth" Bibliothek ist. 
-// Dadurch wurde die Möglichkeit, per Bluetooth hochzuladen abgeschaltet.
+// V0.1.1
+// Calli:bot 2
 
-let KInitialized = 0
-let KLedState = 0
-let KFunkAktiv = 0
-//let KFunkInitialized = 0
-
-enum KMotor {
+enum C2Motor {
     links,
     rechts,
     beide
 }
 
-enum KStop {
+enum C2Stop {
     //% block="auslaufend"
     Frei,
     //% block="bremsend"
     Bremsen
 }
 
-enum KnServo {
+enum C2Servo {
     //% block="Nr.1"
     Servo1,
     //% block="Nr.2"
     Servo2
 }
 
-enum KSensor {
+enum C2Sensor {
     links,
     rechts
 }
 
-enum KSensorStatus {
+enum C2SensorStatus {
     hell,
     dunkel
 }
 
-enum KFunk {
-    an,
-    aus
-}
-
-enum KEinheit {
+enum C2Einheit {
     cm,
     mm
 }
 
-enum KRgbLed {
+enum C2RgbLed {
     //% block="links vorne"
     LV,
     //% block="rechts vorne"
@@ -61,7 +49,7 @@ enum KRgbLed {
     All
 }
 
-enum KRgbColor {
+enum C2RgbColor {
     rot,
     grün,
     blau,
@@ -71,17 +59,17 @@ enum KRgbColor {
     weiß
 }
 
-enum KDir {
+enum C2Dir {
     vorwärts = 0,
     rückwärts = 1
 }
 
-enum KState {
+enum C2State {
     aus,
     an
 }
 
-enum KSensorWait {
+enum C2SensorWait {
     //% block="Entfernung (cm)"
     distanceCm,
     //% block="Entfernung (mm)"
@@ -98,7 +86,7 @@ enum KSensorWait {
     accellZ
 }
 
-enum KCheck {
+enum C2Check {
     //% block="="
     equal,
     //% block="<"
@@ -107,33 +95,44 @@ enum KCheck {
     greaterThan
 }
 
-//% color="#FF0000" icon="\uf013" block="Calli:bot"
-namespace callibot {
 
-    function KInit() {
-        if (KInitialized != 1) {
-            KInitialized = 1;
-            setLed(KMotor.links, KState.aus);
-            setLed(KMotor.rechts, KState.aus);
-            motorStop(KMotor.beide, KStop.Bremsen);
-            setRgbLed(KRgbLed.All, KRgbColor.rot, 0);
-        }
+//% weight=50 color="#FF0000" icon="\uf013" block="Calli:bot 2"
+namespace calliBot2 {
+
+    let c2Initialized = 0;
+    let c2LedState = 0;
+    let c2FunkAktiv = 0;
+    let c2IsBot2 = 0;
+    //let KFunkInitialized = 0
+
+    function init() {
+        if (c2Initialized != 1) {
+            c2Initialized = 1;
+            let buffer = pins.i2cReadBuffer(0x21, 1);
+            if ((buffer[0] & 0x80) != 0){        // Check if it's a CalliBot2
+                c2IsBot2 = 1;
+            }
+            setLed(C2Motor.links, C2State.aus);
+            setLed(C2Motor.rechts, C2State.aus);
+            motorStop(C2Motor.beide, C2Stop.Bremsen);
+            setRgbLed(C2RgbLed.All, 0, 0, 0);
+    }
     }
 
-    function writeMotor(nr: KMotor, direction: KDir, speed: number) {
+    function writeMotor(nr: C2Motor, direction: C2Dir, speed: number) {
         let buffer = pins.createBuffer(3)
-        KInit()
+        init()
         buffer[1] = direction;
         buffer[2] = speed;
         switch (nr) {
-            case KMotor.links:
+            case C2Motor.links:
                 buffer[0] = 0x00;
                 pins.i2cWriteBuffer(0x20, buffer);
                 break;
-            case KMotor.beide:
+            case C2Motor.beide:
                 buffer[0] = 0x00;
                 pins.i2cWriteBuffer(0x20, buffer);
-            case KMotor.rechts:
+            case C2Motor.rechts:
                 buffer[0] = 0x02;
                 pins.i2cWriteBuffer(0x20, buffer);
                 break;
@@ -141,8 +140,8 @@ namespace callibot {
     }
 
     //% speed.min=5 speed.max=100
-    //% blockId=K_motor block="Schalte Motor |%KMotor| |%KDir| mit |%number| %"
-    export function motor(nr: KMotor, direction: KDir, speed: number) {
+    //% blockId=C2_motor block="Schalte Motor |%KMotor| |%KDir| mit |%number| %"
+    export function motor(nr: C2Motor, direction: C2Dir, speed: number) {
         if (speed > 100) {
             speed = 100
         }
@@ -154,9 +153,9 @@ namespace callibot {
     }
 
     //="Stoppe Motor $nr"
-    //% blockId=K_motorStop block="Stoppe Motor |%nr| |%mode"
-    export function motorStop(nr: KMotor, mode: KStop) {
-        if (mode == KStop.Frei) {
+    //% blockId=C2_motorStop block="Stoppe Motor |%nr| |%mode"
+    export function motorStop(nr: C2Motor, mode: C2Stop) {
+        if (mode == C2Stop.Frei) {
             writeMotor(nr, 0, 1);
         }
         else {
@@ -165,8 +164,8 @@ namespace callibot {
     }
 
     //% pos.min=0 pos.max=180
-    //% blockId=K_Servo block="Bewege Servo |%nr| auf |%pos|°"
-    export function servo(nr: KnServo, pos: number) {
+    //% blockId=C2_Servo block="Bewege Servo |%nr| auf |%pos|°"
+    export function servo(nr: C2Servo, pos: number) {
         let buffer = pins.createBuffer(2)
         if (pos < 0) {
             pos = 0
@@ -175,10 +174,10 @@ namespace callibot {
             pos = 180
         }
         switch (nr) {
-            case KnServo.Servo1:
+            case C2Servo.Servo1:
                 buffer[0] = 0x14;
                 break;
-            case KnServo.Servo2:
+            case C2Servo.Servo2:
                 buffer[0] = 0x15;
                 break;
         }
@@ -186,132 +185,137 @@ namespace callibot {
         pins.i2cWriteBuffer(0x20, buffer)
     }
 
-    //% blockId=K_SetLed block="Schalte LED |%KSensor| |%KState"
-    export function setLed(led: KMotor, state: KState) {
+    //% blockId=C2_SetLed block="Schalte LED |%KSensor| |%KState"
+    export function setLed(led: C2Motor, state: C2State) {
         let buffer = pins.createBuffer(2)
-        KInit()
+        init()
         buffer[0] = 0;      // SubAddress of LEDs
         //buffer[1]  Bit 0/1 = state of LEDs
         switch (led) {
-            case KMotor.links:
-                if (state == KState.an) {
-                    KLedState |= 0x01
+            case C2Motor.links:
+                if (state == C2State.an) {
+                    c2LedState |= 0x01
                 }
                 else {
-                    KLedState &= 0xFE
+                    c2LedState &= 0xFE
                 }
                 break;
-            case KMotor.rechts:
-                if (state == KState.an) {
-                    KLedState |= 0x02
+            case C2Motor.rechts:
+                if (state == C2State.an) {
+                    c2LedState |= 0x02
                 }
                 else {
-                    KLedState &= 0xFD
+                    c2LedState &= 0xFD
                 }
                 break;
-            case KMotor.beide:
-                if (state == KState.an) {
-                    KLedState |= 0x03
+            case C2Motor.beide:
+                if (state == C2State.an) {
+                    c2LedState |= 0x03
                 }
                 else {
-                    KLedState &= 0xFC
+                    c2LedState &= 0xFC
                 }
                 break;
         }
-        buffer[1] = KLedState;
+        buffer[1] = c2LedState;
         pins.i2cWriteBuffer(0x21, buffer);
     }
 
     //% intensity.min=0 intensity.max=8
-    //% blockId=K_RGB_LED block="Schalte Beleuchtung |%led| Farbe|%color| Helligkeit|%intensity|(0..8)"
-    export function setRgbLed(led: KRgbLed, color: KRgbColor, intensity: number) {
-        let tColor = 0;
+    //% blockId=C2_RGB_LED block="Schalte Beleuchtung $led rot $red grün $green blau $blue"
+    //% red.min=0 red.max=16
+    //% green.min=0 green.max=16
+    //% blue.min=0 blue.max=16
+    //% inlineInputMode=inline
+    export function setRgbLed(led: C2RgbLed, red: number, green: number, blue: number) {
         let index = 0;
-        let len = 0;
+        let buffer = pins.createBuffer(5)
+        init()
+        if (led != C2RgbLed.All){
+            switch (led) {
+                case C2RgbLed.LH:
+                    index = 2;
+                    break;
+                case C2RgbLed.RH:
+                    index = 3;
+                    break;
+                case C2RgbLed.LV:
+                    index = 1;
+                    break;
+                case C2RgbLed.RV:
+                    index = 4;
+                    break;
 
-        KInit()
-        if (intensity < 0) {
-            intensity = 0;
+            }
+            buffer[0] = 0x03;
+            buffer[1] = index;
+            buffer[2] = red;
+            buffer[3] = green;
+            buffer[4] = blue;
+            pins.i2cWriteBuffer(0x22, buffer);
         }
-        if (intensity > 8) {
-            intensity = 8;
+        else { // all leds, repeat 4 times
+            for (index = 1; index <5; index ++){
+                buffer[0] = 0x03;
+                buffer[1] = index;
+                buffer[2] = red;
+                buffer[3] = green;
+                buffer[4] = blue;
+                pins.i2cWriteBuffer(0x22, buffer);
+            }
         }
-        if (intensity > 0) {
-            intensity = (intensity * 2 - 1) * 16;
-        }
-
-        switch (color) {
-            case KRgbColor.rot:
-                tColor = 0x02
-                break;
-            case KRgbColor.grün:
-                tColor = 0x01
-                break;
-            case KRgbColor.blau:
-                tColor = 0x04
-                break;
-            case KRgbColor.gelb:
-                tColor = 0x03
-                break;
-            case KRgbColor.türkis:
-                tColor = 0x05
-                break;
-            case KRgbColor.violett:
-                tColor = 0x06
-                break;
-            case KRgbColor.weiß:
-                tColor = 0x07
-                break;
-        }
-        switch (led) {
-            case KRgbLed.LH:
-                index = 2;
-                len = 2;
-                break;
-            case KRgbLed.RH:
-                index = 3;
-                len = 2;
-                break;
-            case KRgbLed.LV:
-                index = 1;
-                len = 2;
-                break;
-            case KRgbLed.RV:
-                index = 4;
-                len = 2;
-                break;
-            case KRgbLed.All:
-                index = 1;
-                len = 5;
-                break;
-        }
-        let buffer = pins.createBuffer(len)
-        buffer[0] = index;
-        buffer[1] = intensity | tColor
-        if (len == 5) {
-            buffer[2] = buffer[1];
-            buffer[3] = buffer[1];
-            buffer[4] = buffer[1];
-        }
-        pins.i2cWriteBuffer(0x21, buffer);
-        basic.pause(10);
     }
 
-    //="Liniensensor $sensor"
-    //% blockId K_readLineSensor color="#00C040" block="Liniensensor |%sensor| |%status"
-    export function readLineSensor(sensor: KSensor, status: KSensorStatus): boolean {
+ 
+    //% block="Stoßstange |%sensor| |%status"
+    //% color="#00C040"
+    export function readBumperSensor(sensor: C2Sensor, status: C2State): boolean{
         let result = false
-
         let buffer = pins.i2cReadBuffer(0x21, 1);
-        KInit();
-        if (sensor == KSensor.links) {
+        init();
+        if (sensor == C2Sensor.links) {
+            buffer[0] &= 0x08
+        }
+        if (sensor == C2Sensor.rechts) {
+            buffer[0] &= 0x04
+        }
+
+        switch (status) {
+        case C2State.an:
+            if (buffer[0] != 0) {
+                result = true
+            }
+            else {
+                result = false
+            }
+            break
+        case C2State.aus:
+            if (buffer[0] == 0) {
+                result = true
+            }
+            else {
+                result = false
+            }
+            break
+        }
+        return result;
+    }
+
+
+
+    //% color="#00C040" block="Liniensensor |%sensor| |%status"
+    export function readLineSensor(sensor: C2Sensor, status: C2SensorStatus): boolean {
+        let result = false
+        let buffer = pins.i2cReadBuffer(0x21, 1);
+        init();
+        if (sensor == C2Sensor.links) {
             buffer[0] &= 0x02
         }
-        if (sensor == KSensor.rechts) {
+        if (sensor == C2Sensor.rechts) {
             buffer[0] &= 0x01
         }
         switch (status) {
-            case KSensorStatus.hell:
+            case C2SensorStatus.hell:
                 if (buffer[0] != 0) {
                     result = true
                 }
@@ -319,7 +323,7 @@ namespace callibot {
                     result = false
                 }
                 break
-            case KSensorStatus.dunkel:
+            case C2SensorStatus.dunkel:
                 if (buffer[0] == 0) {
                     result = true
                 }
@@ -331,11 +335,11 @@ namespace callibot {
         return result
     }
 
-    //% blockId=K_entfernung color="#00C040" block="Entfernung |%modus" blockGap=8
-    export function entfernung(modus: KEinheit): number {
+    //% blockId=C2_entfernung color="#00C040" block="Entfernung |%modus" blockGap=8
+    export function entfernung(modus: C2Einheit): number {
         let buffer = pins.i2cReadBuffer(0x21, 3)
-        KInit()
-        if (modus == KEinheit.mm) {
+        init()
+        if (modus == C2Einheit.mm) {
             return 256 * buffer[1] + buffer[2]
         }
         else {
@@ -343,44 +347,46 @@ namespace callibot {
         }
     }
 
-    //% blockId=K_warte color="#0082E6" block="Warte bis |%sensor| |%check| |%value"
-    export function warte(sensor: KSensorWait, check: KCheck, value: number) {
+
+
+    //% blockId=C2_warte color="#0082E6" block="Warte bis |%sensor| |%check| |%value"
+    export function warte(sensor: C2SensorWait, check: C2Check, value: number) {
         let abbruch = 0
         let sensorValue = 0
         while (abbruch == 0) {
             switch (sensor) {
-                case KSensorWait.distance:
-                    sensorValue = entfernung(KEinheit.mm)
+                case C2SensorWait.distance:
+                    sensorValue = entfernung(C2Einheit.mm)
                     break;
-                case KSensorWait.distanceCm:
-                    sensorValue = entfernung(KEinheit.cm)
+                case C2SensorWait.distanceCm:
+                    sensorValue = entfernung(C2Einheit.cm)
                     break;
-                case KSensorWait.accellX:
+                case C2SensorWait.accellX:
                     sensorValue = input.acceleration(Dimension.X)
                     break;
-                case KSensorWait.accellY:
+                case C2SensorWait.accellY:
                     sensorValue = input.acceleration(Dimension.Y)
                     break;
-                case KSensorWait.accellZ:
+                case C2SensorWait.accellZ:
                     sensorValue = input.acceleration(Dimension.Z)
                     break;
-                case KSensorWait.brightness:
+                case C2SensorWait.brightness:
                     sensorValue = input.lightLevel()
                     break;
-                case KSensorWait.temperature:
+                case C2SensorWait.temperature:
                     sensorValue = input.temperature()
                     break;
             }
             switch (check) {
-                case KCheck.equal:
+                case C2Check.equal:
                     if (sensorValue == value)
                         abbruch = 1
                     break;
-                case KCheck.lessThan:
+                case C2Check.lessThan:
                     if (sensorValue < value)
                         abbruch = 1
                     break;
-                case KCheck.greaterThan:
+                case C2Check.greaterThan:
                     if (sensorValue > value)
                         abbruch = 1
                     break;
@@ -388,9 +394,15 @@ namespace callibot {
         }
     }
 
-    //% blockId=K_warte_LSensor color="#0082E6" block="Warte bis Liniensensor |%sensor| = |%status"
-    export function warteLSensor(sensor: KSensor, status: KSensorStatus) {
+    //% color="#0082E6" block="Warte bis Liniensensor $sensor = $status"
+    export function warteLSensor(sensor: C2Sensor, status: C2SensorStatus) {
         while (!(readLineSensor(sensor, status))) {
+        }
+    }
+
+    //% color="#0082E6" block="Warte bis Stoßstange $sensor = $status"
+    export function warteBSensor(sensor: C2Sensor, status: C2State) {
+        while (!(readBumperSensor(sensor, status))) {
         }
     }
 }
